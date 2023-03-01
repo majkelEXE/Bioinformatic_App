50ff0ddb384b538a48e2e9c6890d0960de4bc0fb
KubiO24 <kubakowal2004@gmail.com>
2023-03-01 08:47:20 +0100

    Add more tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,10 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from amino_acid_interpreter.main import check_sequence
+from main import MainWindow
 
 
 class tests(unittest.TestCase):
@@ -30,6 +35,15 @@ class tests(unittest.TestCase):
 
     def test_4(self):
         self.assertEqual(check_sequence("   "), 0, "Wrong result")
+
+    def test_5(self):
+        self.assertEqual(check_sequence("."), 0, False)
+
+    def test_6(self):
+        self.assertEqual(check_sequence("...."), 0, False)
+
+    def test_7(self):
+        self.assertEqual(check_sequence("UAGE"), 0, False)
 
 
 unittest.main()
